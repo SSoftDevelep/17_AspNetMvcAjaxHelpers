83e1a6353ce59749cefbaf212decbdfb17289204
SsoftDevelep <saitemresolmaz@gmail.com>
2017-03-06 16:16:01 +0300

    Asp.Net MVC -AjaxHelpers-BeginForm Method

--- 17_AspNetMvcAjaxHelpers/Controllers/HomeController.cs
+++ 17_AspNetMvcAjaxHelpers/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _17_AspNetMvcAjaxHelpers.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -17,19 +18,56 @@ namespace _17_AspNetMvcAjaxHelpers.Controllers
             return View();
         }
 
+        public ActionResult Index2()
+        {
+            List<ToDoItem> list = null;
+            if (Session["ToDoList"] != null)
+            {
+                list = Session["ToDoList"] as List<ToDoItem>;
+            }
+            else
+            {
+                list = new List<ToDoItem>();
+            }
+            ViewBag.list = list;
+            return View(new ToDoItem());
+        }
+
+        [HttpPost]
+        public PartialViewResult Index2(ToDoItem model)
+        {
+            List<ToDoItem> list = null;
+            if (Session["ToDoList"] != null)
+            {
+                list = Session["ToDoList"] as List<ToDoItem>;
+            }
+            else
+            {
+                list = new List<ToDoItem>();
+            }
+
+            model.Id = Guid.NewGuid();  //model idsi olmadigindan gelen her text icin yeni id urettik.
+            list.Add(model);
+
+            Session["ToDoList"] = list;
+
+            System.Threading.Thread.Sleep(3000);
+
+            return PartialView("_ToDoItemPartialView", model);
+        }
         public PartialViewResult LoadData()
         {
 
             List<string> listem = Session["listem"] as List<string>;
             System.Threading.Thread.Sleep(3000);
-            return PartialView("_VeriListesiPartialView",listem);
+            return PartialView("_VeriListesiPartialView", listem);
 
         }
 
         public MvcHtmlString RemoveData(int id)
         {
             List<string> listem = Session["listem"] as List<string>;
-        
+
             listem.RemoveAt(id);  //gonderilen indexi session listesinden sil.
             Session["listem"] = listem;
             System.Threading.Thread.Sleep(3000);
